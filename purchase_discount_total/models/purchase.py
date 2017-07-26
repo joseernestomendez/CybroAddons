df611d905dd4aab658d2af3cf60cc2459796d2ee
emendez <jose.ernesto.mendez@gmail.com>
2017-07-26 15:58:09 -0400

    adfadfas

--- purchase_discount_total/models/purchase.py
+++ purchase_discount_total/models/purchase.py
@@ -15,7 +15,7 @@ class SaleOrder(models.Model):
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
-                amount_discount += (line.product_uom_qty * line.price_unit * line.discount)/100
+                amount_discount += (line.price_unit * line.discount)/100
             order.update({
                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
